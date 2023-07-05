1d164ba2e47d794f4eba73564ea7f8cf68a210a6
Alexander Engström <aleen361@student.liu.se>
2023-07-05 22:24:13 +0200

    trying to deploy to gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,11 @@ import Contact from "./Pages/Contact"
 import Book from "./Pages/Book"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./Styles/main.css"
-import { createBrowserRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from 'react-router-dom'
+import { createHashRouter, createRoutesFromElements, Outlet, Route, RouterProvider } from 'react-router-dom'
 
 
 function App() {
-  const router = createBrowserRouter(
+  const router = createHashRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
         <Route index element={<Home/>}/>
